5cb1250211e642c413f7cdb137af228453ca33b4
viva-calman <viva.calman@gmail.com>
2015-04-15 17:39:12 +0300

    Fix stupid syntax mistakes

--- lisptodo.lisp
+++ lisptodo.lisp
@@ -41,26 +41,29 @@
 
 ;
 ; Добавление новой записи
-(defgeneric add-new-do ((todolist todolist)
+(defgeneric add-new-do (todolist
 			title)
   (:documentation "Добавление новой записи"))
 
 ;
 ; Обновление текущего номера записи в файле
-(defgeneric update-current-id ((todolist todolist))
+(defgeneric update-current-id (todolist)
   (:documentation "Обновление текущего id записи, для поддержания актуальности данных в объекте")
 			       
 ;
 ; Отображение списка дел
-(defgeneric show-todolist ((todolist todolist)
+(defgeneric show-todolist (todolist
 			   mode)
   (:documentation "Отображение списка дел. Режим определяет, отображаются все дела или только невыполненные"))
 ;
 ; Изменение статуса записи
-(defgeneric change-status ((todoentry todoentry)
+(defgeneric change-status (todoentry
 			   id
 			   status)
-  (:documentation "Изменение статуса записи. 0 - не выполнено, 1 - выполнено, 2 - удалено. Удаленные записи не отображаются при выводе, но сохраняются в объекте до вызова функции чистки"))
+  (:documentation "Изменение статуса записи. 
+0 - не выполнено, 
+1 - выполнено, 
+2 - удалено. Удаленные записи не отображаются при выводе, но сохраняются в объекте до вызова функции чистки"))
 
 
 ;
